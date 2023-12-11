7389ef7537e9f4ce0f67bb11890da053dabe706b
Sunkunwoo <aksiapice@naver.com>
2023-12-11 19:20:36 +0900

    팝업 UI 수정
    팝업 UI 수정

--- Assets/Scripts/UI/Inventory/PopUp.cs
+++ Assets/Scripts/UI/Inventory/PopUp.cs
@@ -5,9 +5,24 @@ public class PopUp : MonoBehaviour
 {
     public TextMeshProUGUI popup_UiTxt;
     public GameObject slots;
-
-    void Start()
+    private void Start()
     {
-        popup_UiTxt.text = $"Using item? {slots.GetComponent<InventorySlot>().item_Name.text}";
+        Vector3 localPosition = transform.position - transform.parent.position;
+
+        // 게임 화면 중앙으로 이동
+        Vector3 centerPosition = new Vector3(Screen.width /2, Screen.height*0.8f, 0);
+        transform.position = centerPosition + localPosition;
+    }
+
+    void Update()
+    {
+        if (slots.GetComponent<InventorySlot>().checkEq != true)
+        {
+            popup_UiTxt.text = $"Using item? {slots.GetComponent<InventorySlot>().item_Name.text}";
+        }
+        else
+        {
+            popup_UiTxt.text = $"Un Using item? {slots.GetComponent<InventorySlot>().item_Name.text}";
+        }
     }
 }
